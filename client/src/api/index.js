d5296c7583728fdb2a2a31e6a791acbe16444395
Raul Duran <rauladuran@hotmail.com>
2022-03-11 05:32:30 +0100

    Add api

---
+++ client/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const url = "http://localhost:5000/phones";
+
+export const fetchPhones = () => axios.get(url);
+export const createPhone = (newPhone) => axios.post(url, newPhone);
+export const updatePhone = (id, updatedPhone) =>
+  axios.patch(`${url}/${id}`, updatedPhone);
+export const deletePhone = (id) => axios.delete(`${url}/${id}`);
