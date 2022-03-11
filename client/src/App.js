827141f6809fe2277d93c7773449d93e836e67f8
Raul Duran <rauladuran@hotmail.com>
2022-03-11 05:33:24 +0100

    Add routes

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import NavigationComp from "./components/Navigation";
+import CreatePhoneView from "./views/CreatePhoneView";
+import EditPhoneView from "./views/EditPhoneView";
+import PhonesView from "./views/PhonesView";
+import PhoneView from "./views/PhoneView";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavigationComp />
+      <Routes>
+        <Route path="/" element={<PhonesView />} />
+        <Route path="/create" element={<CreatePhoneView />} />
+        <Route path="/phone" element={<PhoneView />} />
+        <Route path="/edit" element={<EditPhoneView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
